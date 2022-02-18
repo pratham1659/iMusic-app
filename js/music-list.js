3f79018930693ee2db2c3a6d492d807e1ed83142
Pratham kumar <prathamkumar1985@gmail.com>
2022-02-19 00:03:33 +0530

    iMusic-App Designed By Pratham
    I have Created A Music app By Using HTML , CSS and JavaScript.

---
+++ js/music-list.js
@@ -0,0 +1,33 @@
+// To add more song, just copy the following code and paste inside the array
+let allMusic = [
+  {
+    name: "Ok Jaanu - TitleTrack",
+    artist: "AR Rahman",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ghungroo – War",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Gerua - Dilwale",
+    artist: "Pritam Chakraborty",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Doobey - Gehraiyaan",
+    artist: "Lothika",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "No Lie - Dua Lipa",
+    artist: "Sean Paul",
+    img: "music-5",
+    src: "music-5",
+  },
+];
